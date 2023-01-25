513efb8da582f3814cd16d2ec34eea61f3f8aecd
Joanna Smolińska-Iciek <joannasmolinska91@gmail.com>
2023-01-25 21:04:04 +0100

    Commit2

--- script.js
+++ script.js
@@ -2,8 +2,29 @@ function myFunction() {
   const element = document.getElementById("id01");
   element.innerHTML = "New Heading";
 }
-
+​
 function myColorChange() {
   const element = document.getElementById("id01");
   element.style.color = "red";
+}
+
+function mojaFunkcja(imie) {
+
+  const clients = ["Mango", "Poly", "Ajax"];
+  const clientNameToFind = "Patryk";
+  let message;
+  
+  for (const client of clients) {
+  
+    if (client === imie) {
+      message = "Klient z takim imieniem jest w bazie danych!";
+      break;
+    }
+  
+    message = "Nie znaleźliśmy takiego klienta w bazie danych!";
+  }
+  
+  console.log(message);
+  
+
 }
