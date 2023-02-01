b6b1f6699ea5ef649a59dbeb53133af0782418d2
Joanna Smolińska-Iciek <joannasmolinska91@gmail.com>
2023-02-01 21:04:35 +0100

    newBranch

--- script.js
+++ script.js
@@ -44,3 +44,80 @@ function mojaFunkcja(id) {
     element2.innerHTML = informacja;
   }
 }
+function greet(name) {
+  return `Pozdrawiam ${name}.`;
+}
+
+// Wywołaj funkcję greet i wyświetlij wynik w konsoli
+console.log(greet("Mango")); // Pozdrawiam Mango.
+
+// Wyświetlij funkcję greet w konsoli bez jej wywoływania
+console.log(greet); // ƒ greet() { return `Pozdrawiam ${name}.`; }
+
+function dodawanie(a, b, c) {
+  return a + b + c;
+}
+
+function greet(name2) {
+  console.log(`Pozdrawiam ${name2}.`);
+  return name2;
+}
+
+function registerGuest(name, parametr) {
+  console.log(`Rejestracja gościa ${name}.`);
+
+  parametr(name);
+}
+
+greet("Kiwi");
+console.log(greet("Kiwi"));
+console.log(greet);
+
+registerGuest("Mango", greet);
+
+function processCall(recipient) {
+  const liczba = Math.random() > 0.5;
+
+  console.log(liczba);
+  console.log(!liczba);
+
+  if (!liczba) {
+    console.log(
+      "Abonent " + recipient + " jest niedostępny, zostaw wiadomość."
+    );
+  } else {
+    console.log(`Łączenie z ${recipient}, proszę czekać...`);
+  }
+}
+
+processCall("Mango");
+
+function processCall(recipient, onAvailable, onNotAvailable) {
+  // Symuluj dostępność abonenta za pomocą liczby losowej
+  const isRecipientAvailable = Math.random() > 0.5;
+
+  if (!isRecipientAvailable) {
+    onNotAvailable(recipient);
+    return;
+  }
+
+  onAvailable(recipient);
+}
+
+function takeCall(name) {
+  console.log(`Łączenie z ${name}, proszę czekać...`);
+  // Logika odbierania połączenia
+}
+
+function activateAnsweringMachine(name) {
+  console.log(`Abonent ${name} jest niedostępny, zostaw wiadomość.`);
+  // Logika aktywacji automatycznej sekretarki
+}
+
+function leaveHoloMessage(name) {
+  console.log(`Abonent ${name} jest niedostępny, nagrywamy hologram.`);
+  // Logika nagrywania hologramu
+}
+
+processCall("Mango", takeCall, activateAnsweringMachine);
+processCall("Poly", takeCall, leaveHoloMessage);
